1c87cdc9650c1fb64696512a8c5bd8037ed6fb5a
pkemoi methodius <pkemoimethodius@gmail.com>
2024-06-28 13:15:28 +0300

    Adds the math operation

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply=num1*num2;
+multiply
+
+const random=Math.floor(Math.random() * 10) + 1;
+random
+
+const num3=19;
+const num4=5;
+const mod=num3%num4;
+mod
+
+const max= Math.max(19,20);
+console.log(max);
+
+
+
